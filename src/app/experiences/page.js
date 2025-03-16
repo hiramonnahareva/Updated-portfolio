185e9ec0df9901908fc1765b1cc0fff8ac6432f9
Hiramon Nahar Eva <96996000+hiramonnahareva@users.noreply.github.com>
2025-03-17 03:40:53 +0600

    Update page.js

--- src/app/experiences/page.js
+++ src/app/experiences/page.js
@@ -32,9 +32,11 @@ export default function page() {
      ];
 
   return (
+      
     // <div className='py-40'>
     //     <Experience Experience={experience} />
     // </div>
+      
 
     <div className="container h-[100vh] flex justify-center items-center mx-auto p-40">
       <p className='text-5xl font-bold'>Comming Soon....</p>
